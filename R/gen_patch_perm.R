d1e94920e82812078eb134446e5c9887153bde81
James Geddes <jgeddes@turing.ac.uk>
2017-04-06 14:47:34 +0100

    Finish gen_patch_perm

--- R/gen_patch_perm.R
+++ R/gen_patch_perm.R
@@ -4,6 +4,9 @@
 #' that minimises the mismatch between the patched \code{df1} and \code{df2},
 #' including a permutation cost. Currently the permutation cost is ignored.
 #'
+#' This function uses an assignment problem solver from the package
+#' \code{lpSolve}.
+#'
 #' @param df1 A data frame
 #' @param df2 A data frame
 #' @param cost A "permutation cost"
@@ -20,16 +23,24 @@ gen_patch_perm <- function(df1, df2, cost = 0) {
     n_cols <- ncol(df1)
     if (n_cols != ncol(df2)) stop("df1 and df2 must have the same number of columns")
 
-    mismatches <- array(dim = c(n_cols, n_cols))
+    ## Create a matrix of all pairwise mismatches
+    mismatches <- purrr::map2_dbl(rep(1L:n_cols, times = n_cols),
+                                  rep(1L:n_cols, each = n_cols),
+                                  function(i, j) diffness(df1[, i], df2[, j]))
 
-    for (i in 1L:n_cols) {
-        for (j in i:n_cols) {
-            mismatches[i, j] <- diffness(df1[ ,i], df1[, j])
-            mismatches[j, i] <- mismatches[i, j]
-        }
+    mismatches <- array(mismatches, dim = c(n_cols, n_cols))
+
+    ## Find the optimal match (as a matrix of 0s and 1s)
+    soln <- lpSolve::lp.assign(mismatches)
+
+    ## Recover the permutation if the mismatch is worth the cost
+    mismatch_improvement <- sum(diag(mismatches)) -  soln$objval
+
+    if (mismatch_improvement > cost) {
+        patch_perm(apply(soln$solution, 1, function(v) which(v != 0)))
+    } else {
+        patch_identity()
     }
-
-    mismatches
 }
          
     
