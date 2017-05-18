17e5e4faf4fa80ec7292bbe1955e66ef5a8cb3b0
Timothy Hobson <thobson@turing.ac.uk>
2017-05-18 12:01:22 +0100

    Additional ddiff unit tests

--- tests/testthat/test-ddiff.R
+++ tests/testthat/test-ddiff.R
@@ -16,22 +16,84 @@ test_that("the ddiff function works", {
   df1 <- generate_normal_df(100)
   df2 <- generate_normal_df(100)
 
-  result <- ddiff(df1, df2 = df2)
+  result <- ddiff(df1, df2 = df2, patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99, permute_penalty = 0.4)
   expect_true(is_patch(result, allow_composed = FALSE))
   expect_equal(patch_type(result), "identity")
 
   perm <- as.integer(c(2, 3, 1, 4, 5))
 
-  result <- ddiff(df1, df2 = df2[perm])
+  result <- ddiff(df1, df2 = df2[perm], patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99, permute_penalty = 0.4)
   expect_equal(patch_type(result), "perm")
   expect_identical(get_patch_params(result)[["perm"]], expected = perm)
+
+  ## Test with an affine transformation.
+  set.seed(22)
+  df1 <- generate_normal_df(100)
+  df2 <- generate_normal_df(100)
+
+  df2[[1]] <- 32 + (9/5) * df2[[1]]
+  result <- ddiff(df1, df2 = df2, patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99,
+                  permute_penalty = 0.4, as.list = TRUE)
+
+  # Affine transform is correctly identified, with (approximately) correct
+  # parameters.
+  expect_equal(length(result), expected = 2)
+  expect_equal(patch_type(result[[1]]), expected = "scale")
+  expect_equal(get_patch_params(result[[1]])[["cols"]], expected = 1)
+  expect_equal(get_patch_params(result[[1]])[["scale_factor"]], expected = 2,
+               tolerance = 0.1)
+  expect_equal(patch_type(result[[2]]), "shift")
+  expect_equal(get_patch_params(result[[2]])[["cols"]], expected = 1)
+  expect_equal(get_patch_params(result[[2]])[["shift"]], expected = 32,
+               tolerance = 0.01)
+
+  ## Repeat the affine test with more difficult parameters...
+  set.seed(22)
+  df1 <- generate_normal_df(100)
+  df2 <- generate_normal_df(100)
+
+  df2[[1]] <- 2 + (7/5) * df2[[1]]
+  result <- ddiff(df1, df2 = df2, patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99,
+                  permute_penalty = 0.4, as.list = TRUE)
+
+  # ...still works.
+  expect_equal(length(result), expected = 2)
+  expect_equal(patch_type(result[[1]]), expected = "scale")
+  expect_equal(get_patch_params(result[[1]])[["cols"]], expected = 1)
+  expect_equal(get_patch_params(result[[1]])[["scale_factor"]], expected = 2,
+               tolerance = 0.2)
+  expect_equal(patch_type(result[[2]]), "shift")
+  expect_equal(get_patch_params(result[[2]])[["cols"]], expected = 1)
+  expect_equal(get_patch_params(result[[2]])[["shift"]], expected = 2,
+               tolerance = 0.01)
+
+  ## Repeat again with even more difficult parameters...
+  set.seed(22)
+  df1 <- generate_normal_df(100)
+  df2 <- generate_normal_df(100)
+
+  df2[[1]] <- 2 + (6/5) * df2[[1]]
+  result <- ddiff(df1, df2 = df2, patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99,
+                  permute_penalty = 0.4, as.list = TRUE)
+
+  # This time an erroneous permutation is included.
+  expect_equal(length(result), expected = 3)
+  expect_equal(patch_type(result[[1]]), expected = "scale")
+  expect_equal(patch_type(result[[2]]), expected = "shift")
+  expect_equal(patch_type(result[[3]]), expected = "perm")
 
   ## Test with differing numbers of columns.
   set.seed(22)
   df1 <- generate_normal_df(100)
   df2 <- generate_normal_df(100)
 
-  result <- ddiff(df1, df2 = df2[1:3])
+  result <- ddiff(df1, df2 = df2[1:3], patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99, permute_penalty = 0.4)
   expect_true(is_patch(result, allow_composed = TRUE))
   expect_equal(length(decompose_patch(result)), expected = 2)
   expect_equal(patch_type(decompose_patch(result)[[1]]), "delete")
@@ -42,16 +104,19 @@ test_that("the ddiff function works", {
                    expected = 4L)
   expect_identical(names(result(df1)), expected = names(df2[1:3]))
 
-  # Test with a permutation only (but with differing numbers of columns)
+  ## Test with a permutation only (but with differing numbers of columns)
   perm <- as.integer(c(2, 5, 1, 4, 3))
 
-  result <- ddiff(df1, df2 = df2[perm][1:3])
+  result <- ddiff(df1, df2 = df2[perm][1:3], patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99, permute_penalty = 0.4)
   expect_true(is_patch(result, allow_composed = TRUE))
 
   expect_identical(names(result(df1)), expected = names(df2[perm][1:3]))
 
   insert_col_name <- "INSERT"
-  result <- ddiff(df1[1:3], df2 = df2[perm], insert_col_name = insert_col_name)
+  result <- ddiff(df1[1:3], df2 = df2[perm], patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99, permute_penalty = 0.4,
+                  insert_col_name = insert_col_name)
   expect_true(is_patch(result, allow_composed = TRUE))
 
   expected <- names(df2[perm])
@@ -74,25 +139,29 @@ test_that("the ddiff function works", {
   df1 <- generate_mixed_df(100)
   df2 <- generate_mixed_df(100)
 
-  result <- ddiff(df1, df2 = df2)
+  result <- ddiff(df1, df2 = df2, patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99, permute_penalty = 0.4)
 
   expect_true(is_patch(result, allow_composed = FALSE))
   expect_equal(patch_type(result), "identity")
 
-  # Reducing the penalty associated with a transformation patch changes the result.
-  patch_penalties <- 0.4
-  result <- ddiff(df1, df2 = df2, patch_penalties = patch_penalties)
+  # Reducing the penalty associated with a transformation patch changes the
+  # result: the cost of an affine patch becomes less than improvement in mismatch.
+  result <- ddiff(df1, df2 = df2, patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.4, break_penalty = 0.99, permute_penalty = 0.4)
 
   expect_true(is_patch(result, allow_composed = TRUE))
   expect_false(is_patch(result, allow_composed = FALSE))
 
 
-  # Test with the numeric columns permuted in df1 and a shift.
+  ## Test with the numeric columns permuted in df1 and a shift.
   df2[[1]] <- 4 + df2[[1]]
   perm <- as.integer(c(2, 3, 1, 4, 5))
   df2 <- df2[perm]
 
-  result <- ddiff(df1, df2 = df2, as.list = TRUE)
+  result <- ddiff(df1, df2 = df2, patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99,
+                  permute_penalty = 0.4, as.list = TRUE)
 
   # Test for the expected result.
   expect_equal(length(result), 3)
@@ -101,7 +170,40 @@ test_that("the ddiff function works", {
   expect_equal(patch_type(result[[3]]), "perm")
   expect_equal(get_patch_params(result[[3]])[["perm"]], perm)
 
-  # TODO: test with recodings of categorical data.
+  ## Test with recodings of categorical data.
+  set.seed(22)
+  df1 <- generate_mixed_df(100)
+  df2 <- generate_mixed_df(100)
+
+  df2[[5]] <- ifelse(df2[[5]] == "M", yes = 1L, no = 0L)
+
+  result <- ddiff(df1, df2 = df2, patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99, permute_penalty = 0.4)
+
+  expect_true(is_patch(result, allow_composed = FALSE))
+  expect_identical(patch_type(result, short = TRUE), expected = "recode")
+  expect_identical(get_patch_params(result)[["encoding"]],
+                   expected = c("F" = 0L, "M" = 1L))
+
+  perm <- as.integer(c(2, 3, 1, 4, 5))
+  df2 <- df2[perm]
+
+  result <- ddiff(df1, df2 = df2, patch_generators = list(gen_patch_transform),
+                  patch_penalties = 0.6, break_penalty = 0.99,
+                  permute_penalty = 0.4, as.list = TRUE)
+
+  expect_equal(length(result), expected = 2)
+  expect_identical(patch_type(result[[1]], short = TRUE),
+                   expected = "recode")
+  expect_identical(get_patch_params(result[[1]])[["encoding"]],
+                   expected = c("F" = 0L, "M" = 1L))
+
+  expect_identical(patch_type(result[[2]], short = TRUE),
+                   expected = "perm")
+  expect_identical(get_patch_params(result[[2]])[["perm"]],
+                   expected = perm)
+
+
 
   # TODO: test with data frames containing both factors and integer data (by
   # setting stringsAsFactors = TRUE above).
