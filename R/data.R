4064627c2963f6bc3bef0b65187d54cf4cdabd9f
Timothy Hobson <thobson@turing.ac.uk>
2017-06-21 17:03:02 +0100

    Add UK broadband data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' UK home broadband performance 2014.
+#'
+#' UK home broadband performance for 2014 published by Ofcom.
+#'
+#' @format A data frame with 1971 rows and 31 variables.
+#' @source \url{https://data.gov.uk/dataset/uk-fixed-line-broadband-performance}
+"broadband2014"
+
+#' UK home broadband performance 2015.
+#'
+#' UK home broadband performance for 2015 published by Ofcom.
+#'
+#' @format A data frame with 2802 rows and 66 variables.
+#' @source \url{https://data.gov.uk/dataset/uk-fixed-line-broadband-performance}
+"broadband2015"
