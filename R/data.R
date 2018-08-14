0ca04126b6fd1b2fcbfcc05e14126b902147a7fd
Timothy Hobson <thobson@turing.ac.uk>
2018-08-14 16:11:32 +0100

    Add UCI data sets

--- R/data.R
+++ R/data.R
@@ -1,3 +1,11 @@
+#' UK home broadband performance 2013.
+#'
+#' UK home broadband performance data for 2013 published by Ofcom.
+#'
+#' @format A data frame with 2390 rows and 29 variables.
+#' @source \url{https://data.gov.uk/dataset/uk-fixed-line-broadband-performance}
+"broadband2013"
+
 #' UK home broadband performance 2014.
 #'
 #' UK home broadband performance data for 2014 published by Ofcom.
@@ -14,10 +22,27 @@
 #' @source \url{https://data.gov.uk/dataset/uk-fixed-line-broadband-performance}
 "broadband2015"
 
-#' data from the UCI Machine Learning Repository
+#' Abalone data set
 #'
-#' UK home broadband performance data for 2015 published by Ofcom.
+#' Abalone data set from the UCI Machine Learning Repository.
+#'
+#' @format A data frame with 4177 rows and 9 variables.
+#' @source \url{http://archive.ics.uci.edu/ml/datasets/Abalone}
+"abalone"
+
+#' Heart Disease data set
+#'
+#' Heart Disease data set from the UCI Machine Learning Repository.
 #'
 #' @format A data frame with 303 rows and 14 variables.
-#' @source \url{http://archive.ics.uci.edu/ml}
+#' @source \url{http://archive.ics.uci.edu/ml/datasets/Heart+Disease}
 "heartdisease"
+
+#' Iris data set
+#'
+#' Iris data set from the UCI Machine Learning Repository.
+#'
+#' @format A data frame with 4177 rows and 9 variables.
+#' @source \url{http://archive.ics.uci.edu/ml/datasets/Iris}
+"iris"
+
