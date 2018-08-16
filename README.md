e75d4da1f4d6c8fde98b07f89c257085d7c832de
Timothy Hobson <thobson@turing.ac.uk>
2018-08-16 09:43:26 +0100

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# datadiff
-Datadiff is diff for data
+## Overview
+
+Tabular data sets are common, and many data processing tasks must be repeated on multiple similar data samples. In practice, however, there may be unexpected changes in structure across different batches of data, which are likely to break the analytical pipeline.
+
+Datadiff identifies structural differences between pairs of (related) tabular
+data sets and returns an executable summary (or "patch") which is both a description of the differences and a corrective transformation.
+
+In making comparisons, datadiff considers the following (composable) patch types:
+
+ - column permutation
+ - column insertion
+ - column deletion
+ - column replacement
+ - recoding of categorical data
+ - linear transformation of numerical data
+
+## Installation
+
+``` r
+# Install the most recent release from GitHub:
+# install.packages("devtools")
+devtools::install_github("datadiff", ref = "v0.2.0")
+```
+
+## Usage
+
+Diff two data frames with `ddiff(df1, df2)`. For more information and examples, see the package vignette: `vignette("datadiff")`.
--- vignettes/datadiff.R
+++ vignettes/datadiff.R
@@ -4,6 +4,11 @@ library(datadiff)
 library(tibble)
 # set.seed(1014)
 
+## ---- eval=FALSE---------------------------------------------------------
+#  # Install the most recent release from GitHub:
+#  # install.packages("devtools")
+#  devtools::install_github("datadiff", ref = "v0.2.0")
+
 ## ------------------------------------------------------------------------
 head(as.tibble(broadband2013))
 
@@ -11,12 +16,20 @@ head(as.tibble(broadband2013))
 head(as.tibble(broadband2014))
 
 ## ------------------------------------------------------------------------
-ddiff(broadband2014, broadband2013)
+broadband2013[["ID"]][broadband2013[["ID"]] == "FTTC"] <- NA
+broadband2013$ID <- as.integer(broadband2013$ID)
 
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+strip_non_numeric <- function(x) { gsub("[^0-9\\.]", "", x) }
+broadband2013[["Headline.speed"]] <- as.integer(strip_non_numeric(broadband2013[["Headline.speed"]]))
+broadband2013[["Packet.loss....24.hour"]] <- as.numeric(strip_non_numeric(broadband2013[["Packet.loss....24.hour"]]))
+broadband2013[["Packet.loss....8.10pm.weekday"]] <- as.numeric(strip_non_numeric(broadband2013[["Packet.loss....8.10pm.weekday"]]))
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ------------------------------------------------------------------------
+result <- ddiff(broadband2014, broadband2013)
+
+## ------------------------------------------------------------------------
+print(result, broadband2014)
+
+## ------------------------------------------------------------------------
+head(as.tibble(result(broadband2014)))
 
